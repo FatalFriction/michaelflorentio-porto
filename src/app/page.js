31eab35b32da5051e3d73583fd9b5f9547794f3b
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2023-11-01 06:20:31 +0700

    refactor: remove console log

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import Particleseng from '@/utils/Particleseng';
 import Image from 'next/image';
 
 export default function Home() {
+  
   return (
         <main className="flex min-h-screen items-center justify-between overflow-clip scroll-smooth" data-theme="blueberry">
             <HorizontalCar />
--- src/utils/Particleseng.js
+++ src/utils/Particleseng.js
@@ -63,12 +63,10 @@ const Particleseng = () => {
   }), []);
 
   const particlesInit = useCallback(async (engine) => {
-    console.log(engine);
     await loadFull(engine);
   }, []);
 
   const particlesLoaded = useCallback(async (container) => {
-    console.log(container);
   }, []);
 
   return (
