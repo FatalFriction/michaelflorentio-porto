7d30f732171d5d7ca4f261946eac7e5cbad27efa
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2023-11-01 11:11:04 +0700

    feat: new animation speed dial

--- src/components/UI/speedial/Contactdial.js
+++ src/components/UI/speedial/Contactdial.js
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState } from 'react';
 import FormDial from './FormDial';
+import { motion } from "framer-motion"
 
 const Contactdial = () => {
   const [isFormOpen, setIsFormOpen] = useState(false);
@@ -12,17 +13,30 @@ const Contactdial = () => {
   return (
     <>
       <div className="fixed left-6 bottom-6">
-        <button className="btn btn-circle shadow-2xl btn-neutral hover:btn-success dark:btn-info dark:hover:btn-secondary" onClick={toggleForm}>
+        <div className="" onClick={toggleForm}>
           {isFormOpen ? (
-            <svg xmlns="http://www.w3.org/2000/svg" className="first h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-          ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" className="second h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-          )}
-          </button>
+            <motion.button className="btn btn-circle btn-neutral" initial={{ x: 0 }} whileHover={{ x: 5 , transition: { duration: 0.8 }}} >
+              <svg xmlns="http://www.w3.org/2000/svg" className="first h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+            </motion.button>
+            ) : (
+            <div className='flex flex-wrap items-center'>
+              <motion.button className="btn btn-circle btn-neutral" whileHover={{ width: '280px', transition: { duration: 0.5 } }}>
+                <motion.div className="absolute left-3 p-10" initial={{x: -40 }} whileHover={{x: -24 }}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="second h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                </motion.div>
+              </motion.button>
+              <div className="fixed ml-16 text-white text-lg font-bold">
+                Leave me a Message
+              </div>
+            </div>
+            )}
+          </div>
       </div>
 
       {isFormOpen && (
-        <FormDial/>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 100  }} transition={{ duration: 0.8 }}>
+            <FormDial/>
+        </motion.div>
       )}
     </>
   );
--- src/components/UI/speedial/FormDial.js
+++ src/components/UI/speedial/FormDial.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const FormDial = () => {
   return (
-    <div className='absolute flex flex-col space-y-6 items-center justify-center bottom-6 left-32 w-[29rem] py-6 bg-accent opacity-90 rounded-sm'>
+    <div className='absolute flex flex-col space-y-6 items-center justify-center bottom-6 left-28 w-[29rem] py-6 bg-accent opacity-90 rounded-sm'>
         <span className='text-info font-bold text-3xl opacity-100'>
         Join My Mailing List!
         </span>
         <form method="POST" action="https://www.formbackend.com/f/664decaabbf1c319">
             <div>
             <input type="text" name="email" className='outline outline-2 outline-white mr-4 py-2 px-6 rounded-sm' placeholder='email address'/>
-            <button type="submit" className='bg-info text-white px-6 py-2 rounded-sm'>Subscribe</button>
+            <button type="submit" className='bg-info text-white px-6 py-2 rounded-sm hover:bg-primary'>Subscribe</button>
             </div>
         </form>
     </div>
