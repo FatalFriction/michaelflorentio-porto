a1fc014ef84bb42b7ebe6876df5d778a323f5303
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2023-11-03 05:44:11 +0700

    feat: text3D effect

---
+++ src/components/Text3D/Text3d.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Text3d = ({first, second}) => {
+  return (
+    <div className="text-container text-[8vw] p-4 relative cursor-pointer transform-gpu transform-style-3d transition-transform duration-[0.4s] hover:rotate-x-90">
+        <div className="first-text text-info m-0 hover:-translate-y-full hover:opacity-0 transition-all duration-[0.4s] leading-[8vw]">
+            <p >{first}</p>
+            <p className="first-text text-white absolute top-4 left-2">{first}</p>
+        </div>
+        <div className="second-text text-info m-0 absolute top-0 opacity-0 -rotate-x-90 translate-y-[3vw] origin-bottom-center hover:opacity-100 transition-all duration-[0.4s] leading-[8vw]">
+            <p>{second}</p>
+            <p className="second-text text-accent m-0 absolute top-0 right-2">{second}</p>    
+        </div>
+        </div>
+  )
+}
+
+export default Text3d
