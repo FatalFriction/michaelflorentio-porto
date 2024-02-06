21fb1b1a5bd494b8c060eacfc73c5d26c0f26bed
Michael Florentio <43514974+FatalFriction@users.noreply.github.com>
2024-02-06 14:39:22 +0700

    opts: added dynamic load on heavy components, added suspense to individuals components remove: suspense from page app

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import HorizontalCar from '@/components/UI/Carousel/HorizontalCar';
-import Splash from '@/components/Splash/Splash';
 
 export default function Home() {
   
   return (
-        <Suspense fallback={<Splash/>}>
-          <main className="flex min-h-screen items-center justify-between overflow-clip scroll-smooth bg-transparent" data-theme="blueberry">
-              <HorizontalCar />
-          </main>
-        </Suspense>
+      <main className="flex min-h-screen items-center justify-between overflow-clip scroll-smooth bg-transparent" data-theme="blueberry">
+        <HorizontalCar />
+      </main>
   );
 }
--- src/components/UI/Carousel/HorizontalCar.js
+++ src/components/UI/Carousel/HorizontalCar.js
@@ -1,20 +1,27 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import Card from "@/components/Card/Card";
 import { motion, useTransform, useScroll } from "framer-motion";
 import { useRef } from "react";
-import localFont from 'next/font/local'
-import RobotCanvas from "@/components/LostRobot/RobotCanvas";
-import FirstPanel from "../Panel/FirstPanel";
-import SecondPanel from "../Panel/SecondPanel";
-import ScrollCanvas from "@/components/Scroll/ScrollCanvas";
-import ThirdPanel from "../Panel/ThirdPanel";
+import dynamic from 'next/dynamic';
+
 import { cards } from "@/utils/indexdata";
+import localFont from 'next/font/local'
+
+const RobotCanvas = dynamic(() => import('@/components/LostRobot/RobotCanvas'))
+const ScrollCanvas = dynamic(() => import('@/components/Scroll/ScrollCanvas'))
+
+const SecondPanel = dynamic(() => import('../Panel/SecondPanel'))
+const ThirdPanel = dynamic(() => import('../Panel/ThirdPanel'))
+
+import FirstPanel from "../Panel/FirstPanel";
+
 import Contactdial from '../speedial/Contactdial';
 import Sidebar from '../sidebar/Sidebar';
 import MobilePanelHome from '../Panel/MobilePanelHome';
 import MobilePanelContact from '../Panel/MobilePanelContact';
 import HorizontalDivider from '../HorizontalDivider';
+import Splash from '@/components/Splash/Splash';
 
 const myFont = localFont({
   src: '/HeavyRain.ttf',
@@ -58,16 +65,27 @@ const HorizontalScrollCarousel = ({ x }) => {
       <section className="relative h-[2450vh] bg-transparent whitespace-nowrap scroll-smooth">
         <div className="sticky top-0 flex h-screen items-center">
           <motion.div style={{ x }} className="flex">
-            {cards.map((card) => (
-              card.id === 3 ? <FirstPanel key={card.id} /> :
-                card.id === 4 ? <RobotCanvas key={card.id} /> :
-                  card.id === 5 ? <SecondPanel key={card.id} /> :
-                    card.id === 6 ? <ScrollCanvas key={card.id} /> :
-                      card.id === 7 ? <ThirdPanel key={card.id} /> :
-                        <motion.div key={card.id} className={myFont.className}>
-                          <Card card={card} key={card.id} />
-                        </motion.div>
-            ))}
+          {cards.map((card) => (
+            <motion.div style={{ x }} className="flex" key={card.id}>
+              {card.id === 3 ? (
+                <FirstPanel key={card.id} />
+              ) : card.id === 4 ? (
+                <Suspense fallback={<Splash />}>
+                  <RobotCanvas key={card.id} />
+                </Suspense>
+              ) : card.id === 5 ? (
+                <SecondPanel key={card.id} />
+              ) : card.id === 6 ? (
+                <ScrollCanvas key={card.id} />
+              ) : card.id === 7 ? (
+                <ThirdPanel key={card.id} />
+              ) : (
+                <motion.div className={myFont.className}>
+                  <Card card={card} key={card.id} />
+                </motion.div>
+              )}
+            </motion.div>
+          ))}
           </motion.div>
         </div>
       </section>
